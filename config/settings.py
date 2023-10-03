3da3c23f47f9b7382c2d2eaebbcafc88694ec280
satel31 <blokhnina.tatiana@yandex.ru>
2023-10-04 00:04:21 +0300

    Created model user

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,6 @@ STATIC_ROOT = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Registration of users
+AUTH_USER_MODEL = 'user.User'

ae80d6d18161022f7299cb4f09de22396a425032
satel31 <blokhnina.tatiana@yandex.ru>
2023-10-06 18:58:23 +0300

    Added urls

---
+++ apps/survey/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+app_name = 'survey'
+
+urlpatterns = [
+    # section
+    #path('add_section/', SectionCreateAPIView.as_view(), name='add_section'),
+    #path('sections/', SectionListAPIView.as_view(), name='sections'),
+    #path('sections/<int:pk>/', SectionDetailAPIView.as_view(), name='section'),
+    #path('sections/update/<int:pk>/', SectionUpdateAPIView.as_view(), name='section_update'),
+    #path('sections/delete/<int:pk>/', SectionDeleteAPIView.as_view(), name='section_delete'),
+]
---
+++ apps/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+app_name = 'user'
+
+urlpatterns = [
+    # user
+    #path('add_user/', UserCreateAPIView.as_view(), name='add_user'),
+    #path('users/', UserListAPIView.as_view(), name='users'),
+    #path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user'),
+    #path('users/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    #path('users/delete/<int:pk>/', UserDeleteAPIView.as_view(), name='user_delete'),
+]

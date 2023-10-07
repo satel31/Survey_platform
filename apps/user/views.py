e08829d80ba77071942a06e4d90a0f60ad52366d
satel31 <blokhnina.tatiana@yandex.ru>
2023-10-07 20:58:41 +0300

    Updated views

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,20 +1,20 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from apps.user.models import User
+from apps.user.permissions import IsOwnerPermission
 from apps.user.serializers import UserSerializer, StrangerUserSerializer
-
-
-class UserCreateAPIView(generics.CreateAPIView):
-    serializer_class = UserSerializer
 
 
 class UserListAPIView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = StrangerUserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserDetailAPIView(generics.RetrieveAPIView):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.request.user.email == self.get_object().email:
@@ -24,7 +24,9 @@ class UserDetailAPIView(generics.RetrieveAPIView):
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerPermission]
 
 
 class UserDeleteAPIView(generics.DestroyAPIView):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerPermission]

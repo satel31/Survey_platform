7eb11a3ed80c1784280e9c6d89cc7c5ed11ccb55
satel31 <blokhnina.tatiana@yandex.ru>
2023-10-07 20:58:26 +0300

    Updated urls

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('survey/', include('apps.survey.urls', namespace='survey')),
     path('users/', include('apps.user.urls', namespace='user')),
+    re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
 ]
